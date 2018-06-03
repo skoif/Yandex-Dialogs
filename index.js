8af81adba8eb938dbcf01582c6e9a3e05df99a8a
Alexander Zemskov <spam@sazima.ru>
2018-06-04 02:56:44 +0300

    Version 0.2.0! Text responses works!

--- index.js
+++ index.js
@@ -10,22 +10,10 @@ class Dialog {
         this._app.use(this._bodyParser.json());
 
         this._app.post(this._url, (req,res)=>{
-            let Response = (params, sender)=>{
-                this._params = params;
-                this._sender = sender;
-                this.send = (text, tts, buttons, end_session)=>{
-                    let params = this._params;
-                    params["text"] = text;
-                    params["tts"] = tts;
-                    params["buttons"] = buttons;
-                    if(end_session == null){ params["end_session"] = false; }else{ params["end_session"] = end_session; }
-                    this._sender(params);
-                }
-            };
             let postParams = req.body;
-            let responseParams = {session: postParams.session, version: postParams.version};
-            if(this._binds.original[postParams.request["original_utterance"]] != null ){ this._binds.original[postParams.request["original_utterance"]](postParams, new Response(responseParams, res.send)) }else{
-                if(this._binds.command[postParams.request["command"]] != null){ this._binds.command[postParams.request["command"]](postParams, new Response(responseParams, res.send)) }else{
+            let responseParams = {session: postParams.session, version: postParams.version, response: {}};
+            if(this._binds.original[postParams.request["original_utterance"]] != null ){ this._binds.original[postParams.request["original_utterance"]](postParams, new Response(responseParams, res)) }else{
+                if(this._binds.command[postParams.request["command"]] != null){ this._binds.command[postParams.request["command"]](postParams, new Response(responseParams, res)) }else{
                     if(postParams.request.command === "test" && postParams.request.original_utterance === "test"){ res.send({session: postParams.session, version: postParams.version, response: {text: "test"}}) }else{
                         res.send({session: postParams.session, version: postParams.version, response: {text: this._default_response}})
                     }
@@ -42,29 +30,21 @@ class Dialog {
         if(typeof text === "string"){ this._binds.original[text] = callback; }
         if(typeof text === "object"){ text.forEach((item)=>{ this._binds.original[item] = callback; }); }
     }
+}
 
-
-    _postHandler(req,res){
-        let Response = (params, sender)=>{
-            this._params = params;
-            this._sender = sender;
-            this.send = (text, tts, buttons, end_session)=>{
-                let params = this._params;
-                params["text"] = text;
-                params["tts"] = tts;
-                params["buttons"] = buttons;
-                if(end_session == null){ params["end_session"] = false; }else{ params["end_session"] = end_session; }
-                this._sender(params);
-            }
-        };
-        let postParams = req.body;
-        let responseParams = {session: postParams.request.session, version: postParams.request.version};
-        if(this._binds.original === postParams.request["original_utterance"]){ this._binds.original[postParams.request["original_utterance"]](postParams, new Response(responseParams, res.send)) }else{
-            if(this._binds.command === postParams.request["command"]){ this._binds.command[postParams.request["command"]](postParams, new Response(responseParams, res.send)) }else{
-                if(postParams.request.command === "test" && postParams.request.original_utterance === "test"){ res.send({session: postParams.request.session, version: postParams.request.version, response: {text: "test"}}) };
-            }
+class Response{
+    constructor(params, sender){
+        this._params = params;
+        this._senderapp = sender;
+        this.send = (text, tts, buttons, end_session)=>{
+            let params = this._params;
+            params["response"]["text"] = text;
+            params["response"]["tts"] = tts;
+            params["response"]["buttons"] = buttons;
+            if(end_session == null){ params["end_session"] = false; }else{ params["end_session"] = end_session; }
+            this._senderapp.send(params);
         }
-    }
+    };
 }
 
 module.exports = Dialog;
